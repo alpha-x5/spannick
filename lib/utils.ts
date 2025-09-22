e5d5e3b20f56551f4b0ab22a0d1338696f705f33
alpha-x5 <rl5022@srmist.edu.in>
2025-09-22 12:08:54 +0530

    Create missing lib/utils.ts file

---
+++ lib/utils.ts
@@ -0,0 +1,23 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+*/
+
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function getFriendlyErrorMessage(error: unknown, fallbackMessage: string): string {
+  if (error instanceof Error) {
+    return error.message;
+  }
+  
+  if (typeof error === 'string') {
+    return error;
+  }
+  
+  return fallbackMessage;
+}
